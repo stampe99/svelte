eecd0084764c2696a6c214c5d09033812261c13c
stampe <92384039+stampe99@users.noreply.github.com>
2022-05-10 19:50:31 +0200

    Add files via upload

---
+++ src/pages/_fallback.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { url } from '@roxi/routify'
+</script>
+
+<style>
+  .huge {
+    font-size: 12rem;
+  }
+  .e404 {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    margin: 10px;
+  }
+</style>
+
+<div class="e404">
+  <div class="huge">404</div>
+  <div class="big">Uh oh... We cannot find the page you're searching for... <br>$ {location.href} <br>
+  <!-- link to the parent folder of _fallback.svelte -->
+  <a href={$url('../')}>Go back</a>
+  </div>
+</div>
---
+++ src/pages/_layout.svelte
@@ -0,0 +1,6 @@
+<slot />
+<script>
+    import "../global.css";
+    import "flowbite/dist/flowbite.css";
+    import { Plane } from 'svelte-loading-spinners'
+  </script>
